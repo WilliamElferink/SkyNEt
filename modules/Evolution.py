c7bdad4d102b4c0e6d2be72d47d925ef264f3ffe
Annefleur <annefleuruitzetter@hotmail.com>
2019-05-15 16:57:12 +0200

    working

--- modules/Evolution.py
+++ modules/Evolution.py
@@ -7,7 +7,7 @@ import time
 
 class GenePool(object):
 
-    def __init__(self, config_obj, dataset):
+    def __init__(self, config_obj):
 
         self.config_obj = config_obj
         self.genes = config_obj.genes
@@ -15,7 +15,6 @@ class GenePool(object):
         self.generange = config_obj.generange
         self.pool = np.zeros((self.genomes, self.genes))
         for i in range(0,self.genes):
-            np.random.seed((i+1) * (10 + dataset))
             self.pool[:,i] = np.random.uniform(self.generange[i][0], self.generange[i][1], size=(self.genomes,))
         np.random.seed(seed=None)
         #input scaling between 0 and 1 
@@ -59,7 +58,6 @@ class GenePool(object):
         '''Mutate all genes but the first partition[0] with a triangular 
         distribution between 0 and 1 with mode=gene. The chance of mutation is 
         config_obj.mutationrate'''
-        np.random.seed(seed=None)
         mask = np.random.choice([0, 1], size=self.pool[self.partition[0]:].shape, 
                                   p=[1-self.config_obj.mutationrate, self.config_obj.mutationrate])
         mutatedpool = np.zeros((self.genomes-self.partition[0], self.genes))
@@ -85,7 +83,6 @@ class GenePool(object):
         return generange[0] + gene * (generange[1] - generange[0])
 
     def AddNoise(self):
-        np.random.seed(seed=None)
         '''Add Gaussian noise to the fittest partition[1] genes'''
         for i in range(0,self.genes):
             self.newpool[sum(self.partition[:1]):sum(self.partition[:2]), i] = self.pool[:self.partition[1],i] + 0.02 * np.random.uniform(self.generange[i][0], self.generange[i][1], size=(self.partition[1],))
@@ -98,7 +95,6 @@ class GenePool(object):
             
 
     def CrossoverFitFit(self):
-        np.random.seed(seed=None)
         '''Perform crossover between the fittest :partition[2] genomes and the
         fittest 1:partition[2]+1 genomes'''
         mask = np.random.randint(2, size=(self.partition[2], self.genes))
@@ -106,7 +102,6 @@ class GenePool(object):
                 + (np.ones(mask.shape) - mask) * self.pool[1:self.partition[2]+1])
 
     def CrossoverFitRandom(self):
-        np.random.seed(seed=None)
         '''Perform crossover between the fittest :partition[3] genomes and random
         genomes'''
         mask = np.random.randint(2, size=(self.partition[3], self.genes))
@@ -114,14 +109,12 @@ class GenePool(object):
                 + (np.ones(mask.shape) - mask) * self.pool[np.random.randint(self.genomes, size=(self.partition[3],))])
 
     def AddRandom(self):
-        np.random.seed(seed=None)
         '''Generate partition[4] random genomes'''
         for i in range(0,self.genes):
             self.newpool[sum(self.partition[:4]):,i] = np.random.uniform(self.generange[i][0], self.generange[i][1], 
                      size=(self.partition[4],))
         
     def RemoveDuplicates(self):
-        np.random.seed(seed=None)
         '''Check the entire pool for any duplicate genomes and replace them by 
         the genome put through a triangular distribution'''
         for i in range(self.genomes):
