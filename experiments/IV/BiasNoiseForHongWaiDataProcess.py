ea6a14518c33e5e2d58e4d43c3dd44f85383c1fa
chentao07 <38476860+chentao07@users.noreply.github.com>
2021-03-04 10:31:52 +0100

    999

--- experiments/IV/BiasNoiseForHongWaiDataProcess.py
+++ experiments/IV/BiasNoiseForHongWaiDataProcess.py
@@ -7,21 +7,22 @@ import numpy as np
 import os
 from scipy import signal
 import matplotlib.pyplot as plt
+import timeit
 
 
-path = r"C:/Users/ChenT/Desktop/Results"
+path = r"C:/Users/ChenT/Desktop/Results/TCTO_B_HBPS_oxideremoved/T/"
 directories = os.listdir(path+'/Data')
-pppath = path+'/Processed'
+pppath = path+'/Processed/'
 if not os.path.exists(pppath):
     os.makedirs(pppath)
 
-fftlen=256
-fftoverlap=192
+fftlen=1024*10
+fftoverlap=1024*9
 
 for file in directories:
     print(file)
     t=float(file.split('_')[0][0:-1])
-    data=np.load(path+file)
+    data=np.load(path+'/Data/'+file)
     gatevoltage=data['Gatevoltage']
     sourcevoltage=data['Sourcevoltage']
     outputcurrent=data['Outputcurrent']
@@ -30,39 +31,43 @@ for file in directories:
     sl=fssl[1]
     mavgcurrent=np.zeros((len(gatevoltage),len(sourcevoltage)))
     mpxx1norm=np.zeros((len(gatevoltage),len(sourcevoltage)))
-    mnoisepower=np.zeros((len(gatevoltage),len(sourcevoltage),fftlen/2+1))
+    mnoisepower=np.zeros((len(gatevoltage),len(sourcevoltage),int(fftlen/2+1)))
     vgi=0
+    start = timeit.timeit()
     for vg in gatevoltage:
         vsi=0
         for vs in sourcevoltage:
-            x=outputcurrent[vgi,vsi,:]#[fs:]
-            f,pxx=signal.welch(x,fs,nperseg=fftlen,noverlap=fftoverlap)
+            x=outputcurrent[vgi,vsi,:][fs:]
+            f,pxx=signal.welch(x-np.average(x),fs,nperseg=fftlen,noverlap=fftoverlap)
             f1index=(np.abs(f-1)).argmin()
-            pxx1norm=pxx[f1index]/np.average(x)
-            mnoisepower[vgi,vsi,:]=pxx
+            pxx1norm=pxx[f1index]/np.square(np.average(x))
+            mnoisepower[vgi,vsi,:]=pxx/np.square(np.average(x))
             mavgcurrent[vgi,vsi]=np.average(x)
             mpxx1norm[vgi,vsi]=pxx1norm
-            plt.semilogy(f,pxx)
-            plt.ylim([-9,-3])
-            plt.xlabel('frequency [Hz]')
-            plt.ylabel('PSD [nA^2/Hz]')
-            plt.show()
+#            plt.semilogy(f,pxx)
+#            plt.ylim([-9,-3])
+#            plt.xlabel('frequency [Hz]')
+#            plt.ylabel('PSD [nA^2/Hz]')
+#            plt.show()
             vsi=vsi+1
+        end = timeit.timeit()
+        print('Time left: '+str(len(gatevoltage)*(end-start))+' seconds')
         vgi=vgi+1
     filename=pppath+'processed'+file.split('_')[0]+'.npz'
     np.savez(filename, currentdata=mavgcurrent, noise1hz=mpxx1norm, noisepower=mnoisepower, temperature=t)
 
-directories = os.listdir(path+'/Processed')
+directories = os.listdir(path+'/Processed/')
 if len(directories)<2:
     print('Not enough temperature data points')
 else:
-    data1=np.load(path+directories[0])
+    data1=np.load(pppath+directories[0])
     mavgcurrent1=data1['currentdata']
-    mpxx1norm=data1['noisepower']
-    data2=np.load(path+directories[1])
+    mpxx1norm=data1['noise1hz']
+    data2=np.load(pppath+directories[1])
     mavgcurrent2=data2['currentdata']
-    TCR=(mavgcurrent1-mavgcurrent2)/mavgcurrent1
-    fig,ax=plt.subplot(2)
-    ax[0].imshow(TCR, cmap='hot', interpolation='nearest')
-    ax[1].imshow(mpxx1norm, cmap='hot', interpolation='nearest')
+    tcr=(mavgcurrent1-mavgcurrent2)/(mavgcurrent1*5)
+    plt.plot(sourcevoltage,mavgcurrent[6,:])
     plt.show()
+    plt.loglog(f,mnoisepower[6,60,:])
+    plt.show()
+    
